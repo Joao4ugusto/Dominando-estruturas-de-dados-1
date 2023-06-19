522fb8838381397a4604329e6d544d0b48986256
João <joaoaugusto311016@gmail.com>
2023-06-19 15:00:37 -0300

    19-06

---
+++ Listas-Encadeadas-Simples/linked_lista.c
@@ -0,0 +1,10 @@
+#include "linkedi_list.h"
+
+typedef struct _SNo{
+  int val;
+  struct _sNo *proximo;
+} SNo;
+
+typedef struct _Linked_List {
+  sNo *primeiro;
+} Linked_list;
---
+++ Listas-Encadeadas-Simples/linkedi_list.h
@@ -0,0 +1,7 @@
+#ifndef LINKED_LIST_H
+#define LINLED_LIST_H
+
+typedef struct _SNo SNo;
+typedef struct _Linked_list  Linked_list;
+
+#endif
